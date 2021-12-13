ff849bb75a05f0b903dcba2a1589371f9ab1e303
Kelvin Esekhile <kelvinoesekhile@gmail.com>
2021-12-13 19:07:45 +0100

    initial commit

---
+++ apps/housematefinder-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'housematefinder-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'housematefinder-app';
+}
